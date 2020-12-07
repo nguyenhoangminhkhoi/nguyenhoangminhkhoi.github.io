e52f8c9f6acaa8b708715e648dd591cb74de1c8d
nguyenhoangminhkhoi <nguyenkhoi28100@gmail.com>
2020-12-07 17:16:13 +0700

    CSS file

---
+++ homePage.js
@@ -0,0 +1,7 @@
+var menu = [
+    "Home",
+    "What is Git ?",
+    "What is VCS ?",
+    "How does Git work ?",
+    "What benefits does Git have ?"
+]
---
+++ personal website library.js
@@ -0,0 +1,13 @@
+var GIToverview  = [
+    {
+        heading : "What is git ?",
+        paragraph: "Git is a Distributed Version Control System - DVCS, arguably, it is one of the most commonly used systems today . Git provides each developer with a separated repository, containing a complete history of changes. ",
+        img : "",
+    },
+    {
+        heading : "What is Version Control System - VCS ?",
+        paragraph: "To give you more material for a better insight into this system, I will provide you with an explanation about how the so-called Version Control System works . <br> Version Control System - VCS will manage and store all the files of the entire project and also the modifying history of each file. With each modification, a version will be created",
+        img : "", 
+    }    
+]
+        
